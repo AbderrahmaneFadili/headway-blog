dec117db18d8467497edd760c3c5eadaf40de2d3
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-07-07 11:52:27 +0100

    Upload Avatar helper

---
+++ server/helper/uploadAvatarHelper.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+console.log(multer);
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "uploads/avatars");
+  },
+  filename: (req, file, callback) => {
+    callback(null, `avatar-${Date.now()}-` + file.originalname);
+  },
+});
+
+const multerFilter = (req, file, callback) => {
+  if (
+    file.mimetype == "image/png" ||
+    mimetype == "image/jpg" ||
+    mimetype == "image/jpeg"
+  ) {
+    callback(null, true);
+  } else {
+    callback(null, false);
+    return callback(new Error("Only .png, .jpg and .jpeg format allowed!"));
+  }
+};
+
+exports.upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
